4c313b13a1516d97bd2276c83644c0ed717d7075
urmi-21 <mrbains21@gmail.com>
2020-06-27 14:46:18 -0500

    update script check duplicate sort data

--- scripts/parse.py
+++ scripts/parse.py
@@ -10,13 +10,21 @@ import yaml
 import glob
 import os
 import sys
+from operator import itemgetter
 
-datasets=[]
-resources=[]
-#add headers
-datasets.append('|'.join(['Date','Title','Description','Download','#Samples','#COVID','Type'])+'\n'+'|'.join(['---','---','---','---','---','---','---']))
-resources.append('|'.join(['Resource','Description']))
-resources.append('|'.join(['---','---']))
+
+datasets=[] #is a list of lists, each inner-list is a row
+resources=[] #is a list of lists
+#headers
+datasets_h1=['S.No.','Date','Title','Description','Download','#Samples','#COVID','Type']
+datasets_h2=['---','---    ','---','---','---','---','---','---']
+resources_h1=['S.No.','Resource','Description']
+resources_h2=['---','---','---']
+
+
+#keep track of added data
+added_dataset=[]
+added_resources=[]
 
 #get a list of all yaml files in data
 def get_files(directory,suffix):
@@ -25,12 +33,31 @@ def get_files(directory,suffix):
     result=glob.glob(path)
     return result
 
+def check_duplicate_keys(yaml):
+    '''
+    check for duplicate keys in yaml
+    return true if there are duplicate keys
+    '''
+    with open(yaml) as f:
+        data=f.read().splitlines()
+        #print(data)
+    keys=[]
+    duplicate=False
+    for l in data:
+        if not (l.startswith('#') or l.startswith(' ') or l.startswith('-') or l.startswith('\n')):
+            if (l):
+                #print ('keyfound:'+l)
+                if l in keys:
+                    duplicate=True
+                    print('File: {} found duplicate key {}'.format(yaml,l))
+                keys.append(l) 
+    return duplicate
 
 def parse_yml(filepath):
     all_dict=yaml.safe_load(open(filepath))
 
     for key, d in all_dict.items():
-        #print (key,":", value)
+        #print (key,":")
         #decide if entry is rna-seq or resource
         if len(d) >3 :
             #this is RNA-Seq data
@@ -38,6 +65,10 @@ def parse_yml(filepath):
             #validate keys
             try:
                 title=d['title']
+                if title in added_dataset:
+                    print("SKIPPING: {} already exists".format(title))
+                    continue   
+                added_dataset.append(title)
                 link=d['link']
                 desc=d['description']
                 date=d['date']
@@ -52,7 +83,8 @@ def parse_yml(filepath):
                 covid=d['samples']['covid']
                 title=mdlink(title,link)
                 download='/'.join([mdlink(geoacc,geolink),mdlink(sraacc,sralink),mdlink(otheracc,otherlink)])
-                result='|'.join([str(date),title,desc,download,str(total),str(covid),typeseq])    
+                #S.No. will be assigned later after sorting
+                result=['0',str(date),title,desc,download,str(total),str(covid),typeseq]    
                 datasets.append(result)
                 #return result
             except:
@@ -64,9 +96,13 @@ def parse_yml(filepath):
             #this is resource
             try:
                 title=d['title']
+                if title in added_resources:
+                    print("SKIPPING: {} already exists".format(title))
+                    continue   
                 link=d['link']
                 desc=d['description']
-                result='|'.join([mdlink(title,link),desc])                
+                result=['0',mdlink(title,link),desc]
+                added_resources.append(title)
                 resources.append(result)
             except:
                 print('2 Error parsing:'+filepath+' key:'+key)
@@ -84,8 +120,11 @@ def mdlink(text,link):
 
 yml_files=get_files('data','yaml')
 for f in yml_files:
-    #print (f)
+    #ignore template.yaml file
     if not 'template.yaml' in f:
+        if check_duplicate_keys(f):
+            print('ERROR: Please fix duplicate yaml keys in file: '+f+'. Exiting...')
+            sys.exit(1)
         parse_yml(f)
 
 #after parsing and creating 'datasets' and 'resources' dict, make README file
@@ -93,15 +132,37 @@ for f in yml_files:
 #print('\n'.join(datasets))
 #print('\n'.join(resources))
 
+#sort datasets by date and assign S.No.
+datasets=sorted(datasets, key=itemgetter(1),reverse=True) #sort by date
+resources=sorted(resources, key=itemgetter(1)) # sort by title
+dind=1
+#generate S.No.
+for l in datasets:
+    l[0]=str(dind)
+    dind+=1
+rind=1
+for l in resources:
+    l[0]=str(rind)
+    rind+=1
 
-#convert to tab
-#data_table=datasets_to_tab(datasets)
-#resources_table=resources_to_tab(resources)
+#add headers
+datasets.insert(0,datasets_h2)
+datasets.insert(0,datasets_h1)
+resources.insert(0,resources_h2)
+resources.insert(0,resources_h1)
 
 #write to file
 print('writing to file')
-data_table='\n'.join(datasets)
-resources_table='\n'.join(resources)
+#conver to md format
+datasets_md=[]
+for l in datasets:
+    datasets_md.append('|'.join(l))
+resources_md=[]
+for l in resources:
+    resources_md.append('|'.join(l))
+
+data_table='\n'.join(datasets_md)
+resources_table='\n'.join(resources_md)
 target='README2.md'
 sep='## ##'
 #get content before tables e.g. introduction section
@@ -112,7 +173,7 @@ content='\n'.join(content).split(sep)[0]
 
 #write newly parsed tables along with  content
 f=open(target,'w')
-f.write('\n\n'.join([content+'\n'+sep,'## COVID-19-RNA-Seq-datasets',data_table,'\n\n## COVID-19-RNA-Seq Resources',resources_table]))
+f.write('\n\n'.join([content+sep,'## COVID-19-RNA-Seq-datasets',data_table,'\n\n## COVID-19-RNA-Seq Resources',resources_table]))
 print('Done!')
 
 
